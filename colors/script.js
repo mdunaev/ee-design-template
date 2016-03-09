26eb340b77e5f987e6815b8d5390d46f7fa059a5
Mikhail Dunaev <dunaev.m@gmail.com>
2016-03-09 18:55:05 +0300

    add colors selector

---
+++ colors/script.js
@@ -0,0 +1,28 @@
+palette.crossOrigin = "Anonymous";
+
+
+function rgbToHex(r, g, b){
+    r = r.toString(16);
+    g = g.toString(16);
+    b = b.toString(16);
+
+    if (r.length == 1) r = '0' + r;
+    if (g.length == 1) g = '0' + g;
+    if (b.length == 1) b = '0' + b;
+
+    return (r + g + b).toUpperCase();
+}
+
+
+$('#palette').click(function(e) {
+    if(!this.canvas) {
+
+        this.canvas = $('<canvas />')[0];
+        this.canvas.crossOrigin = "Anonymous";
+        this.canvas.width = this.width;
+        this.canvas.height = this.height;
+        this.canvas.getContext('2d').drawImage(this, 0, 0, this.width, this.height);
+    }
+      var pixelData = this.canvas.getContext('2d').getImageData(event.offsetX, event.offsetY, 1, 1).data;
+      alert( '#'+rgbToHex(pixelData[0], pixelData[1], pixelData[2]) );
+  });
